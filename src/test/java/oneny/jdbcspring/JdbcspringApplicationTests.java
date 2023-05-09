2ce08571731b01aaf15d6c404466493f0e27af30
oneny <gjwodnjs1996@gmail.com>
2023-05-09 22:31:34 +0900

    first commit

---
+++ src/test/java/oneny/jdbcspring/JdbcspringApplicationTests.java
@@ -0,0 +1,13 @@
+package oneny.jdbcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
