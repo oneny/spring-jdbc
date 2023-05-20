1e7bec386f45814686e815ca75e303674ba206b4
oneny <gjwodnjs1996@gmail.com>
2023-05-20 15:57:03 +0900

    add: db 락

--- src/main/java/oneny/jdbcspring/connection/ConnectionConst.java
+++ src/main/java/oneny/jdbcspring/connection/ConnectionConst.java
@@ -5,4 +5,6 @@ public abstract class ConnectionConst {
   public static final String URL = "jdbc:h2:tcp://localhost/~/test";
   public static final String USERNAME = "sa";
   public static final String PASSWORD = "";
+
+
 }
